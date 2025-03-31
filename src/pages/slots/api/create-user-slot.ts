478b4c570d470a6fbb2e5da197f5ab4857433782
Alex <alex.campos.calvo@gmail.com>
2025-03-31 23:39:19 +0200

    Excel like functionality

--- src/pages/slots/api/create-user-slot.ts
+++ src/pages/slots/api/create-user-slot.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from 'astro'
-import { db, Upsert_History, User_Slot } from 'astro:db'
+import { db, eq, Slot, Upsert_History, User_Slot } from 'astro:db'
 import { generateId } from 'lucia'
 import moment from 'moment'
 import 'moment/locale/es'
@@ -39,19 +39,84 @@ export const POST: APIRoute = async ({ request, locals }) => {
             teacher_id: locals.session.userId
           })
           .run()
-      }
 
-      await db
-        .insert(User_Slot)
-        .values({
-          id: generateId(15),
-          user_id: body.user_id,
-          slot_id: body.to_slot,
-          default: body.to_date ? false : true,
-          date: body.to_date ? body.to_date : null
-        })
-        .run()
-      return new Response(null, { status: 200 })
+        await db
+          .insert(User_Slot)
+          .values({
+            id: generateId(15),
+            user_id: body.user_id,
+            slot_id: body.to_slot,
+            default: false,
+            date: body.to_date
+          })
+          .run()
+        return new Response(null, { status: 200 })
+      } else if (!body.to_date && body.user_id && body.to_slot) {
+        const slot = await db.select().from(Slot).where(eq(Slot.id, body.to_slot)).all()
+
+        if (!slot || !slot[0]) {
+          return new Response('Invalid slot id', { status: 400 })
+        }
+
+        const this_week = moment().startOf('week').days(slot[0].week_day).format('YYYY-MM-DD')
+        const first_week = moment()
+          .startOf('week')
+          .add(1, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+        const second_week = moment()
+          .startOf('week')
+          .add(2, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+        const third_week = moment()
+          .startOf('week')
+          .add(3, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+
+        await db
+          .insert(User_Slot)
+          .values([
+            {
+              id: generateId(15),
+              user_id: body.user_id,
+              slot_id: body.to_slot,
+              default: true,
+              date: null
+            },
+            {
+              id: generateId(15),
+              user_id: body.user_id,
+              slot_id: body.to_slot,
+              default: false,
+              date: this_week
+            },
+            {
+              id: generateId(15),
+              user_id: body.user_id,
+              slot_id: body.to_slot,
+              default: false,
+              date: first_week
+            },
+            {
+              id: generateId(15),
+              user_id: body.user_id,
+              slot_id: body.to_slot,
+              default: false,
+              date: second_week
+            },
+            {
+              id: generateId(15),
+              user_id: body.user_id,
+              slot_id: body.to_slot,
+              default: false,
+              date: third_week
+            }
+          ])
+          .run()
+        return new Response(null, { status: 200 })
+      }
     } catch (e) {
       console.error(e)
       return new Response(e?.message, { status: 500 })
--- src/pages/slots/api/delete-user-slot.ts
+++ src/pages/slots/api/delete-user-slot.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from 'astro'
-import { and, db, eq, Swap_History, Upsert_History, User_Slot } from 'astro:db'
+import { and, db, eq, inArray, Slot, Swap_History, Upsert_History, User_Slot } from 'astro:db'
 import { generateId } from 'lucia'
+import moment from 'moment'
+import 'moment/locale/es'
 
 interface Body {
   id: string
@@ -15,13 +17,13 @@ export const DELETE: APIRoute = async ({ request, locals }) => {
     }
 
     try {
-      const user_slot = await db
-        .select()
-        .from(User_Slot)
-        .where(and(eq(User_Slot.id, body.id), eq(User_Slot.default, false)))
-        .all()
+      const user_slot = await db.select().from(User_Slot).where(eq(User_Slot.id, body.id)).all()
 
-      if (user_slot && user_slot.length > 0 && user_slot[0] && user_slot[0].date) {
+      if (!user_slot || !user_slot[0]) {
+        return new Response('Invalid user_slot id', { status: 400 })
+      }
+
+      if (user_slot[0].date && !user_slot[0].default) {
         await db
           .insert(Upsert_History)
           .values({
@@ -34,6 +36,53 @@ export const DELETE: APIRoute = async ({ request, locals }) => {
             teacher_id: locals.session.userId
           })
           .run()
+      } else if (user_slot[0].default) {
+        const slot = await db.select().from(Slot).where(eq(Slot.id, user_slot[0].slot_id)).all()
+
+        if (!slot || !slot[0]) {
+          return new Response('Invalid slot id', { status: 400 })
+        }
+
+        const this_week = moment().startOf('week').days(slot[0].week_day).format('YYYY-MM-DD')
+        const first_week = moment()
+          .startOf('week')
+          .add(1, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+        const second_week = moment()
+          .startOf('week')
+          .add(2, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+        const third_week = moment()
+          .startOf('week')
+          .add(3, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+        const fourth_week = moment()
+          .startOf('week')
+          .add(4, 'week')
+          .days(slot[0].week_day)
+          .format('YYYY-MM-DD')
+
+        const user_slots = await db
+          .select()
+          .from(User_Slot)
+          .where(
+            and(
+              eq(User_Slot.user_id, user_slot[0].user_id),
+              eq(User_Slot.slot_id, user_slot[0].slot_id),
+              eq(User_Slot.default, false),
+              inArray(User_Slot.date, [this_week, first_week, second_week, third_week, fourth_week])
+            )
+          )
+          .all()
+
+        if (user_slots && user_slots.length > 0) {
+          const user_slots_ids = user_slots.map((user_slot) => user_slot.id)
+          await db.delete(Swap_History).where(inArray(Swap_History.user_slot, user_slots_ids)).run()
+          await db.delete(User_Slot).where(inArray(User_Slot.id, user_slots_ids)).run()
+        }
       }
 
       await db.delete(Swap_History).where(eq(Swap_History.user_slot, body.id)).run()
